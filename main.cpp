280a494f521c7fd8a64f5b828b792ae6beacf1f3
zsms <i@zsms.me>
2023-09-20 11:57:51 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     std::cout << "666" << std::endl;
+
+    double good = 200.00;
+
+    auto result = sizeof good;
+
+    std::cout << result << std::endl;
+
+    std::cout << "end" << std::endl;
+
+    char s[4] = "222";
+
+    std::cout << sizeof(s) - 1 << std::endl;
+
     return 0;
 };
